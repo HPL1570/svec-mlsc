a488d16365ca5e01ac8093af1e07d698e31cdd41
Patcha Hemanth Lakshman <137917960+HPL1570@users.noreply.github.com>
2024-01-28 15:55:46 +0530

    Update settings.py 2

--- MLSC_main/settings.py
+++ MLSC_main/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'https://cdn.jsdelivr.net/gh/HPL1570/mlscfiles/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'staticfiles')
 ] 
