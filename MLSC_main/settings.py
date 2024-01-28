1b4387af59008e78cf43b9ec844335977bd0c00a
HPL1570 <hemanthpatcha55555@gmail.com>
2024-01-28 21:52:37 +0530

    added-images

--- MLSC_main/settings.py
+++ MLSC_main/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vonk!_5(fq=+aw6t@4_2lzduncq-nz)$jxmv+9f&klinj#+pez'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
